f172afc1524d32884e352784b46f0ceb6f51576d
joadisorn <joadisorn331@gmail.com>
2021-03-06 14:34:41 +0700

    Initial commit

---
+++ flutter_application_1/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp();
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+          );
+        }
+
+      }
+
+      class HomeScreen extends StatefulWidget {
+        HomeScreen({Key key}) : super(key: key);
+
+        @override
+        _HomeScreenState createState() => _HomeScreenState();
+      }
+
+      class _HomeScreenState extends State<HomeScreen> {
+        @override
+        Widget build(BuildContext context) {
+          return Container(
+             child: Scaffold(
+
+             ),
+          );
+        }
+      }
