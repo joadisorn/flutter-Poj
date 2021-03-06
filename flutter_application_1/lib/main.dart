bac0aa883b66b7b753d89fff18997574d1dc6b5a
joadisorn <joadisorn331@gmail.com>
2021-03-06 14:41:44 +0700

    Update main.dart

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp();
+  runApp(
+    MyApp()
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -26,8 +28,34 @@ class MyApp extends StatelessWidget {
         Widget build(BuildContext context) {
           return Container(
              child: Scaffold(
-
-             ),
+      drawer: Drawer(
+        child: Center(
+          child: Text("Hello drawer"),
+        ),
+      ),
+      appBar: AppBar(
+        title: Text("my first project"),
+      ),
+      body: Center(
+        child: Text("Hello world"),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.phone),
+            label: "phone",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: "star",
+          )
+        ],
+      ),
+    ),
           );
         }
       }
